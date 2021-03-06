abc5715e6c264808f3774aa4f7ffc8e653bcaf29
Mark Halstead <mark.d.halstead@gmail.com>
2021-03-06 15:07:13 -0700

    init

--- src/js/stocklists/Components/StockApp.jsx
+++ src/js/stocklists/Components/StockApp.jsx
@@ -7,6 +7,23 @@ import StockGet from './StockGet.jsx';
 import Presets from './Presets.jsx';
 import Intro from './Intro.jsx';
 // var Graph = require('./Graph');
+
+
+// import { Form, Button, Card, Alert } from "react-bootstrap"
+// import React from "react"
+ // import Signup from "./Signup.js"
+// import { Container } from "react-bootstrap"
+// import { AuthProvider } from "../contexts/AuthContext"
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// import Dashboard from "./Dashboard"
+// import Login from "./Login"
+// import PrivateRoute from "./PrivateRoute"
+// import ForgotPassword from "./ForgotPassword"
+// import UpdateProfile from "./UpdateProfile"
+//
+
+
+
 import {
   FaCannabis, FaKeyboard, FaLightbulb, FaBookmark, FaRing, FaPen, FaIndustry,
   FaSun, FaMoneyBill, FaPrescription, FaToiletPaper, FaPlusSquare
